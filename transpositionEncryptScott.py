3dfa1d977644baf48642a70a6dd4584687daa026
Scott Blanck <scottblanck@gmail.com>
2020-06-04 09:37:07 -0400

    adding first code

---
+++ transpositionEncryptScott.py
@@ -0,0 +1,31 @@
+import pyperclip
+
+def main():
+    myMessage = 'Common sense is not so common.'
+    myKey = 13
+
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    print(ciphertext + '|')
+
+    pyperclip.copy(ciphertext)   # copy to the clipboard
+
+def encryptMessage(key, message):
+    ciphertext = [''] * key      # each string represents a column of text
+
+    #Loop through each column
+    for col in range(key):
+        pointer = col
+
+        while pointer < len(message):   # Repeat until past end of message
+            ciphertext[col] += message[pointer]  # Grab a single char
+
+            pointer += key   # jump the pointer to the next char
+
+    return ''.join(ciphertext)  # return a single string joined together
+
+
+# if run directly then call the main() function
+if __name__ == '__main__':
+    main()
+    
